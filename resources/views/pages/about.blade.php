871f595ffb4842166fbae94e0353059348f265c8
Eddie Kao <eddie@digik.com.tw>
2020-09-20 17:39:32 +0800

    Day 7 - Layouts

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,17 +1,22 @@
-<h1>Hi</h1>
+@extends('layouts/application')
 
-@if(count($heroes) > 0)
-    @foreach($heroes as $hero)
-        @if($loop->odd)
-            <li>
-                *{{ $hero }}
-            </li>
-        @else
-            <li>
-                {{ $hero }}
-            </li>
-        @endif
-    @endforeach
-@endif
+@section('title', '為你自己學 Laravel')
 
+@section('main')
+    <h1>Hi</h1>
+
+    @if(count($heroes) > 0)
+        @foreach($heroes as $hero)
+            @if($loop->odd)
+                <li>
+                    *{{ $hero }}
+                </li>
+            @else
+                <li>
+                    {{ $hero }}
+                </li>
+            @endif
+        @endforeach
+    @endif
+@endsection
 
